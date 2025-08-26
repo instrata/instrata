f4ce16ddc80a7d87b709299c8fddf58ff081b00c
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-08-26 13:48:44 +0200

    updated Cargo.toml with plugins to persist the window-state and register global shortcuts

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,8 @@
-
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
