cec4df964e20fa268561f6206efeb2a3878f943a
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-08-27 11:38:56 +0200

    added plugin-fs to access the file-system

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,15 @@
 mod commands;
-use tauri::{Manager};
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             #[cfg(desktop)]
             {
-                let _ = app.get_webview_window("main")
+                let _ = app
+                    .get_webview_window("main")
                     .expect("no main window")
                     .set_focus();
             }
