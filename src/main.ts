038701c4ba126b7e38fc36759b967cf98f4e67b6
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-08-30 21:55:22 +0200

    added ctrl+r for development

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,12 @@ import { router } from "@/router.ts";
 createApp(AppWrapper)
     .use(router)
     .mount("#app");
+
+if (import.meta.env.DEV) {
+    document.addEventListener("keydown", (event) => {
+        if ((event.ctrlKey && event.key === "r") || event.key === "F5") {
+            event.preventDefault();
+            window.location.reload();
+        }
+    });
+}
