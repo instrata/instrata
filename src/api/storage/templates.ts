a15fffc6ed8996ec63b7c7269ad5b433dfaef3fc
Barakudum <68517540+Barakudum@users.noreply.github.com>
2025-10-14 12:37:38 +0200

    feat(i18n): templates now have i18n support

--- src/api/storage/templates.ts
+++ src/api/storage/templates.ts
@@ -3,8 +3,8 @@ import { exists, readDir, readTextFile } from "@tauri-apps/plugin-fs";
 
 export type TemplateMeta = {
     id: string
-    displayName: string
-    description?: string
+    displayName: string | Record<string, string>
+    description?: string | Record<string, string>
     version?: string
     author?: string
     formats: string[]
@@ -32,7 +32,17 @@ export async function findTemplates(): Promise<TemplateMeta[]> {
         findCustomTemplates(),
     ]))
         .flat()
-        .sort((a, b) => a.displayName.localeCompare(b.displayName));
+        .sort((a, b) => {
+            const aName = (typeof a.displayName === "string"
+                ? a.displayName
+                : a.displayName["en"])
+                || a.id;
+            const bName = (typeof b.displayName === "string"
+                ? b.displayName
+                : b.displayName["en"])
+                || a.id;
+            return aName.localeCompare(bName);
+        });
 }
 
 async function findBuiltinTemplates(): Promise<TemplateMeta[]> {
@@ -52,11 +62,35 @@ async function findWithinDirectory(dir: string): Promise<TemplateMeta[]> {
         entries
             .filter(entry => entry.isDirectory)
             .map(async entry => {
-                const metaFile = await join(dir, entry.name, "metadata.json");
-                const content = await readTextFile(metaFile);
-                const parsed = JSON.parse(content);
+                const fallbackMeta: TemplateMeta = {
+                    id: entry.name,
+                    displayName: entry.name,
+                    formats: [],
+                };
+
+                const metadataFile = await join(dir, entry.name, "metadata.json");
+                if (!await exists(metadataFile)) {
+                    return {
+                        ...fallbackMeta,
+                        description: "missing template metadata",
+                    } as TemplateMeta;
+                }
+                const content = await readTextFile(metadataFile);
+                let parsed;
+                try {
+                    parsed = JSON.parse(content);
+                } catch (error) {
+                    return {
+                        ...fallbackMeta,
+                        description: (error instanceof Error) ? `${error.name}: ${error.message}` : `${error}`,
+                    } as TemplateMeta;
+                }
                 return {
+                    // ensure some default value exist
+                    ...fallbackMeta,
+                    // update with the actual information
                     ...parsed,
+                    // and ensure this is correct
                     id: entry.name,
                 } as TemplateMeta;
             })
--- src/components/pages/settings/menu-templates/TemplateCard.vue
+++ src/components/pages/settings/menu-templates/TemplateCard.vue
@@ -6,10 +6,38 @@ import { ExternalLink } from "@/components/ui2/external-link";
 import { Icon, type IconifyIcon } from "@iconify/vue";
 import { getIconData } from "@iconify/utils";
 import { icons as SOCIAL_ICONS } from "@iconify-json/simple-icons";
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 
-defineProps<{
+const props = defineProps<{
   metadata: TemplateMeta,
 }>();
+const { locale } = useI18n();
+
+const displayName = computed(() => {
+  const displayName = props.metadata.displayName;
+  const fallback = props.metadata.id;
+  if (typeof displayName === "string")
+    return displayName || fallback;
+  if (typeof displayName === "object" && displayName !== null && displayName !== undefined)
+    return (displayName[locale.value] ?? displayName["en"]) || fallback;
+  return fallback;
+});
+const description = computed(() => {
+  const description = props.metadata.description;
+  if (typeof description === "string")
+    return description;
+  if (typeof description === "object" && description !== null && description !== undefined)
+    return (description[locale.value] ?? description["en"]);
+  return undefined;
+});
+
+// material-symbols:error-outline
+const unknownSocialFallback: IconifyIcon = {
+  "width": 24,
+  "height": 24,
+  "body": "<path fill=\"currentColor\" d=\"M12 17q.425 0 .713-.288T13 16t-.288-.712T12 15t-.712.288T11 16t.288.713T12 17m-1-4h2V7h-2zm1 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8\" />"
+}
 </script>
 
 <template>
@@ -18,7 +46,7 @@ defineProps<{
       <div class="flex items-center gap-2 font-semibold text-lg leading-tight">
         <LucideFileCode class="shrink-0" />
         <span class="min-w-0 line-clamp-1">
-          {{ metadata.displayName }}
+          {{ displayName }}
         </span>
         <Badge v-if="metadata.version" variant="secondary">
           {{ metadata.version }}
@@ -33,8 +61,8 @@ defineProps<{
         </template>
       </div>
 
-      <div v-if="metadata.description" class="text-sm text-muted-foreground whitespace-normal break-words">
-        {{ metadata.description }}
+      <div v-if="description" class="text-sm text-muted-foreground whitespace-normal break-words">
+        {{ description }}
       </div>
 
       <div v-if="metadata.author" class="text-xs text-muted-foreground break-words">
@@ -45,7 +73,7 @@ defineProps<{
       <template v-for="link in metadata.links" :key="link">
         <ExternalLink :href="link.url" :title="link.tooltip ?? link.social">
           <div class="bg-secondary p-0.5 rounded-full cursor-pointer">
-            <Icon :icon="getIconData(SOCIAL_ICONS, link.social) as IconifyIcon" :title="link.tooltip ?? link.social" class="size-5" />
+            <Icon :icon="getIconData(SOCIAL_ICONS, link.social) ?? unknownSocialFallback" :title="link.tooltip ?? link.social" class="size-5" />
           </div>
         </ExternalLink>
       </template>
